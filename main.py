a7fc9704ada5666fc8922e976ff601dbdb92ee3d
johnhangen <jthangen@gmail.com>
2025-01-27 17:33:46 -0500

    updated where getting data from bc slow

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ import torch.optim as optim
 import torch
 import wandb
 import torchvision
+import deeplake
+
+print(torch.cuda.is_available())
+print(torch.cuda.device_count())
+print(torch.cuda.current_device())
+print(torch.cuda.get_device_name(0)) 
 
 def main():
     config = Config.load_config(
@@ -31,9 +37,12 @@ def main():
     )
     
     #Create datasets
-    dataset = torchvision.datasets.ImageFolder(
-      root='/content/drive/MyDrive/Projects/data/nabirds/nabirds/images',
-      transform=transforms.Compose([
+    #dataset = torchvision.datasets.ImageFolder(
+    #  root='/content/drive/MyDrive/Projects/data/nabirds/nabirds/images',
+    #  transform=
+    #)
+
+    train_transform = transforms.Compose([
                             transforms.RandomHorizontalFlip(),
                             transforms.RandomRotation(10),
                             transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
@@ -41,55 +50,57 @@ def main():
                             transforms.ToTensor(),
                             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
                         ])
-    )
     
-    train_size = int(0.8 * len(dataset))
-    val_size = (len(dataset) - train_size)//2 + 1
-    test_size = (len(dataset) - train_size)//2
+    val_transform = transforms.Compose([
+                            transforms.Resize((224, 224)),
+                            transforms.ToTensor(),
+                            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+                        ])
+    
+    #train_size = int(0.8 * len(dataset))
+    #val_size = (len(dataset) - train_size)//2 + 1
+    #test_size = (len(dataset) - train_size)//2
+
+
+    #train_dataset, val_dataset, test_dataset = random_split(dataset, [train_size, val_size, test_size])
+    
+    train_dataset = deeplake.load('hub://activeloop/nabirds-dataset-train')
+    val_dataset = deeplake.load('hub://activeloop/nabirds-dataset-val')
     dataset_sizes = {
-        'train': train_size,
-        'val': val_size,
-        'test': test_size
+        'train': len(train_dataset),
+        'val': len(val_dataset),
     }
 
-    train_dataset, val_dataset, test_dataset = random_split(dataset, [train_size, val_size, test_size])
-    
     train_dataloader = DataLoader(
             train_dataset,
+            transforms=train_transform,
             batch_size=config.DataLoader.BatchSize,
             shuffle=True,
             num_workers=config.DataLoader.num_workers,
             pin_memory=True,
+            prefetch_factor=4,
             persistent_workers=True
             )
     
     val_dataloader = DataLoader(
             val_dataset,
+            transforms=val_transform,
             batch_size=config.DataLoader.BatchSize,
             shuffle=True,
             num_workers=config.DataLoader.num_workers,
             pin_memory=True,
-            persistent_workers=True
-            )
-    
-    test_dataloader = DataLoader(
-            test_dataset,
-            batch_size=config.DataLoader.BatchSize,
-            shuffle=True,
-            num_workers=config.DataLoader.num_workers,
-            pin_memory=True,
+            prefetch_factor=4,
             persistent_workers=True
             )
     
     dataloaders = {
         'train': train_dataloader,
-        'val': val_dataloader,
-        'test': test_dataloader
+        'val': val_dataloader
     }
     
     # model
     ResNet = BirdClassifierResNet(
-        num_classes=len(dataset.find_classes('/content/drive/MyDrive/Projects/data/nabirds/nabirds/images')[0]),
+        num_classes=555,
         pretrained=config.Model.Pretrained
     )
     ResNet.load(r'/content/drive/MyDrive/Projects/ResNet.pt')
